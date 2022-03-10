6f8e8a89f99847d2e24ecd78491190cf0e300789
macuacuafrancisco@gmail.com <francisco@Franciscos-iMac.local>
2022-03-10 15:06:14 +0200

    MAIN INTERFACE STYLES DONE

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,25 @@
 import React from 'react'
-import {View} from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
+import { Container, Title, Form, Input, Submit } from './styles'
 
 export default function Main() {
   return (
-    <View/>
+    <Container>
+        <Title>Repositorios</Title>
+
+        <Form>
+              <Input 
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  placeholder="Procurar repositorio ..."
+              />
+
+          <Submit onPress={()=>{}}> 
+                <Icon name="add" size={22} color="#FFF" />
+          </Submit>
+
+          </Form>
+    </Container>
   )
 }
